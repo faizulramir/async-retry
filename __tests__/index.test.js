075c998b018f0ecc90ed883060c8c8c1d144b82d
faizulramir <faizul.ramir@gmail.com>
2023-02-19 05:43:02 +0800

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,17 @@
-const retry = require('../index');
+const retry = require('../index.js');
 
 test('retries the function until it succeeds or reaches the maximum number of retries', async () => {
-    let count = 0;
-    const fn = () => new Promise((resolve, reject) => {
-      count++;
-      if (count === 3) {
-        resolve('success');
-      } else {
-        reject(new Error('failed'));
-      }
-    });
-  
-    const result = await retry(fn, { retries: 5, delay: 100 });
-    expect(result).toEqual('success');
-    expect(count).toEqual(3);
+  let count = 0;
+  const fn = () => new Promise((resolve, reject) => {
+    count++;
+    if (count === 3) {
+      resolve('success');
+    } else {
+      reject(new Error('failed'));
+    }
+  });
+
+  const result = await retry(fn, { retries: 5, delay: 100 });
+  expect(result).toEqual('success');
+  expect(count).toEqual(3);
 });
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function retry(fn, options) {
-    const attempt = 1;
+    let attempt = 1;
     return new Promise((resolve, reject) => {
       fn(resolve, reject).then(resolve).catch(error => {
         if (attempt < options.retries) {
